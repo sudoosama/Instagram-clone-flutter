21e57f50edfa146151aed15abec3df231410e5aa
OsamaxD1 <hassanosama16b@gmail.com>
2020-03-31 05:07:12 +0500

    Home Screen with login complete credential added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,31 +12,99 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   bool isAuth=false;
+  PageController pageController;
+  int pageIndex=0;
 
+  @override
+  void initState() {
+
+    pageController=PageController();
+    pageController=PageController(
+   //   initialPage: 2
+    );
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account){
+      handleSignIn(account);
+    },onError: (err){
+      print('Error Signing in: $err');
+    });
+    //Navigator.pushReplacementNamed(context, '/home');  // This is the culprit
+
+
+    //Reauthenticate when user is sign in
+    googleSignIn.signInSilently(suppressErrors: false).then((account){
+      handleSignIn(account);
+    }).catchError((err){
+      print('Error Signing in: $err');
+    });
+  }
+  handleSignIn(GoogleSignInAccount account){
+    if(account !=null){
+      print('User Signed In: $account');
+      setState(() {
+        isAuth=true;
+      });
+    }
+    else{
+      setState(() {
+        isAuth=false;
+      });
+    }
+  }
+
+  @override
+  void dispose(){
+    pageController.dispose();
+    super.dispose();
+  }
 
   login(){
     googleSignIn.signIn();
   }
-  @override
-  void initState() {
-    super.initState();
-    googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account){
-      if(account !=null){
-        print('User Signed In: $account');
-        setState(() {
-          isAuth=true;
-        });
-      }
-      else{
-        setState(() {
-          isAuth=false;
-        });
-      }
-    });
-    //Navigator.pushReplacementNamed(context, '/home');  // This is the culprit
+  logout(){
+    googleSignIn.signOut();
   }
-  buildAuthScreen(){
-    return Text('Authticated');
+  onPageChanged(int pageIndex){
+    setState(() {
+      this.pageIndex=pageIndex;
+    });
+  }
+  onTap(int pageIndex){
+    pageController.jumpToPage(pageIndex );
+  }
+  Scaffold buildAuthScreen(){
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+         // Timeline(),
+         // ActivityFeed(),
+          //Upload(),
+          //Search(),
+          //Profile(),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged ,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: pageIndex,
+        onTap: onTap,
+        activeColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.whatshot),),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications_active),),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_camera,size: 35.0,),),
+          BottomNavigationBarItem(icon: Icon(Icons.search),),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle),),
+
+
+        ],
+      ),
+    );
+    //return RaisedButton(
+   //   child: Text('Logout'),
+    //  onPressed: logout,
+  //  );
   }
   buildUnAuthScreen(){
     return Scaffold(

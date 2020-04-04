71477620cc97569b9f669aff7fdea9165c35a5e9
OsamaxD1 <hassanosama16b@gmail.com>
2020-04-04 23:28:36 +0500

    Upload page upadate

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -125,7 +125,7 @@ class _HomeState extends State<Home> {
             onPressed: logout,
           ),
           ActivityFeed(),
-          Upload(),
+          Upload(currentUser: currentUser),
           TimeLine(),
           Search(),
           Profile(),
--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,15 +1,208 @@
+import 'dart:io';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:socialmedia/model/user.dart';
 
 class Upload extends StatefulWidget {
+  final User currentUser;
+
+  Upload({this.currentUser});
+
   @override
   _UploadState createState() => _UploadState();
 }
 
 class _UploadState extends State<Upload> {
+  File file;
+
+  handleTakePhoto() async {
+    Navigator.pop(context);
+    File file = await ImagePicker.pickImage(
+        source: ImageSource.camera, maxHeight: 675.0, maxWidth: 960.0);
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  handleChooseFromGallery() async {
+    Navigator.pop(context);
+    File file = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  selectImage(parentContext) {
+    return showDialog(
+        context: parentContext,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text('Create Post'),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text('Photo with Camera'),
+                onPressed: handleTakePhoto,
+              ),
+              SimpleDialogOption(
+                child: Text('Image with Gallery'),
+                onPressed: handleChooseFromGallery,
+              ),
+              SimpleDialogOption(
+                child: Text('Cancel'),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          );
+        });
+  }
+
+  Container buildSplashScreen() {
+    return Container(
+      color: Colors.teal.withOpacity(0.6),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SvgPicture.asset(
+            'assets/images/upload.svg',
+            height: 260.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text(
+                  "Upload Image",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+                color: Colors.deepOrange,
+                onPressed: () => selectImage(context)),
+          )
+        ],
+      ),
+    );
+  }
+
+  clearImage() {
+    setState(() {
+      file = null;
+    });
+  }
+
+  Scaffold buildUploadForm() {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.white70,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onPressed: clearImage,
+          ),
+          title: Text(
+            "Caption Post",
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: [
+            FlatButton(
+              onPressed: () => print('pressed'),
+              child: Text(
+                "Post",
+                style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0),
+              ),
+            ),
+          ]),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 220.0,
+            width: MediaQuery.of(context).size.width*0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16/9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: FileImage(file),
+                    )
+
+                  ),
+                ),
+              ),
+            ),
+          )
+          ,
+          Padding(
+            padding: EdgeInsets.only(top: 10.0),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(
+                widget.currentUser.photoUrl
+              ),
+
+                ),
+            title: Container(
+                width: 250.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Write a caption..."
+                        ,
+                    border: InputBorder.none,
+                  ),
+              )
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.pin_drop,color: Colors.orange,size: 35.0),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Where was this photo taken?",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          )
+          ,
+          Container(
+            width: 200.0,
+            height: 100.0,
+            alignment: Alignment.center,
+            child: RaisedButton.icon(
+                label: Text("Use current location",
+                style: TextStyle(
+                  color: Colors.white
+                ),
+
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                color: Colors.blue,
+                onPressed:()=>print("get user location"),
+            icon: Icon(
+              Icons.my_location,
+              color: Colors.white,
+            ),),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return file == null ? buildSplashScreen() : buildUploadForm();
   }
 }
